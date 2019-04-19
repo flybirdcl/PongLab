d044a510b881e53650382fbb0a2b383dd4406d93
liuc5363 <liuc5363@CA-SU-F106-06.sduhsd.lan>
2019-04-19 09:17:04 -0700

    Added this class

---
+++ src/ponglabs/Collidable.java
@@ -0,0 +1,12 @@
+package PongLabs;
+
+/**
+ *
+ * @author liuc5363
+ */
+public interface Collidable {
+    boolean didCollideLeft(Object obj);
+    boolean didCollideRight(Object obj);
+    boolean didCollideTop(Object obj);
+    boolean didCollideBottom(Object obj);
+}
