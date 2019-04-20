5f9e1be6ec8d44468eacfe4ee1cbdcf73ef8031c
flybirdcl <cassidy.liu2@gmail.com>
2019-04-19 17:19:05 -0700

    Finished

---
+++ src/ponglabs/BlinkyBall.java
@@ -0,0 +1,45 @@
+package Pong;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.Random;
+
+class BlinkyBall extends Ball {
+
+    //constructors
+    public BlinkyBall() {
+        super();
+    }
+
+    public BlinkyBall(int x, int y) {
+        super(x,y);
+    }
+
+    public BlinkyBall(int x, int y, int wid, int ht) {
+        super(x,y,wid,ht);
+    }
+
+    public BlinkyBall(int x, int y, int wid, int ht, int xSpd, int ySpd) {
+        super(x,y,wid,ht,Color.BLACK,xSpd,ySpd);
+    }
+
+    public BlinkyBall(int x, int y, int wid, int ht, Color col, int xSpd, int ySpd) {
+        super(x,y,wid,ht,col,xSpd,ySpd);
+
+    }
+
+    public Color randomColor() {
+        int r = (int)(Math.random()*25);		//use Math.random()
+        int g = (int)(Math.random()*25);
+        int b = (int)(Math.random()*25);
+        return new Color(r, g, b);
+    }
+
+    public void moveAndDraw(Graphics window) {
+        draw(window, Color.WHITE);
+        setX(getX() + super.getXSpeed());
+        //setY
+        setY(getY() + super.getYSpeed());
+        //draw the ball at its new location
+        draw(window, randomColor());
+    }
+}
